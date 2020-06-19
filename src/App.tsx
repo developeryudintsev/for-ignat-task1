a5748e27ddc2f176d49a30801dedaf0787a2fd19
login <kakvospitat@gmail.com>
2020-06-19 18:52:21 +0300

    for-ignat-task1

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,20 @@ import './App.css';
 import Telegram from "./task1/Telegram";
 
 function App() {
+
+  let arQualities=[
+    {id:1,qualities:'JS',className:'myQualities'},
+    {id:2,qualities:'Bootstrap',className:'myQualities1'},
+    {id:3,qualities:'React',className:'myQualities'},
+    {id:4,qualities:'TS',className:'myQualities'},
+  ]
+
+  //
+  // let filtered=arQualities.filter(f=>f.className!=='myQualities1')
+
   return (
     <div className="App">
-    <Telegram name='Sasha' surName='Yudintsev' age={11}/>
+    <Telegram name='Sasha' surName='Yudintsev'  arQualities={arQualities}/>
     </div>
   );
 }
--- src/task1/Telegram.tsx
+++ src/task1/Telegram.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import style from './TelegramStyles.module.css';
 
-type ITelegram={
-    name:string,
-    surName:string,
-    age:number
+type ITelegram = {
+    name: string,
+    surName: string,
+    arQualities: Array<InArray>
 }
 
-function Telegram(props:ITelegram) {
-return(
-    <div className={style.wrapper}>
-        <div className={style.telega}>
-            <p></p>
-            <div>{props.name}</div>
-            <div>{props.surName}</div>
-            <hr/>
-            <p></p>
-            <div>{props.age}</div>
+type InArray = {
+    id: number,
+    qualities: string,
+    className:string,
+}
+
+function Telegram(props: ITelegram) {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.telega}>
+                <p></p>
+                <div>{props.name}</div>
+                <div>{props.surName}</div>
+                <hr/>
+                {props.arQualities.map(m => <div key={m.id} className={style.myQualities1}>My
+                    qualities:+ {m.qualities}</div>)}
+
+            </div>
         </div>
-    </div>
 
-)
+    )
 }
-
+// <div className={styles.tile + ' ' + this.props.color}>
 export default Telegram
