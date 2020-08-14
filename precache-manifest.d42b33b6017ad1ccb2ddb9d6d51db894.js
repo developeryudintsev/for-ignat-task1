6f47418afee04d681c519c62ea832354c6eded45
login <kakvospitat@gmail.com>
2020-08-14 08:37:27 +0300

    Updates

---
+++ precache-manifest.d42b33b6017ad1ccb2ddb9d6d51db894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339bcadf48b6a1a792f5ac373d4bdb1b",
+    "url": "/for-ignat-task1/index.html"
+  },
+  {
+    "revision": "6673aa15b859bff199bc",
+    "url": "/for-ignat-task1/static/css/main.5971fb52.chunk.css"
+  },
+  {
+    "revision": "0723348b486483ddb67e",
+    "url": "/for-ignat-task1/static/js/2.afc95897.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/for-ignat-task1/static/js/2.afc95897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6673aa15b859bff199bc",
+    "url": "/for-ignat-task1/static/js/main.32d65a63.chunk.js"
+  },
+  {
+    "revision": "27ed14d3f57aa4010026",
+    "url": "/for-ignat-task1/static/js/runtime-main.5f2fd733.js"
+  }
+]);
